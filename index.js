9435970ad0f777c9a8a839cf2a5e8b9adf3d9f04
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-26 13:00:30 -0400

    Clearing out index.js

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-// const burgersList = document.getElementById('burgers-list')
-
-// fetch('http://localhost:3000/burgers')
-// .then(response => response.json())
-// .then(burgers => {
-//     burgers.forEach(addBurgertoBurgerList)     
-// })
-
-// function addBurgertoBurgerList(burger){
-//     const liElement = document.createElement('li')
-
-//     const idmeElement = document.createElement('h3')
-//     idmeElement.textContent = `Name: ${burger.name}`;
-
-//     const nameElement = document.
-
-//     liElement.appenChild(idElement)
-//     liElement.appenChild(nameElement)
-//     liElement.appendChild(imageElement)
-//     burgersList.appendChild(liElement)
-// }
 
 // some testing line
