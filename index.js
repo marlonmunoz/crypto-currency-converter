06962f949a32309c141b483262a15e906c7ecdb5
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-26 17:27:07 -0400

    Created top five using JS.

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
 // some testing line
+
+const topFiveList = document.getElementById("top-five")
+
+fetch("https://api.coincap.io/v2/assets")
+.then(response => response.json())
+.then(cryptoCurrencies => {
+    cryptoList = cryptoCurrencies.data
+    const topFive = cryptoList.filter(currency => {
+        return Number(currency.rank) <= 5
+    })
+    console.log(topFive)
+    topFive.forEach(currency =>{
+        const spanElement = document.createElement('span')
+        const divElement = document.createElement('div')
+        const paragraphElement = document.createElement('p')
+
+        divElement.className = "borders"
+        paragraphElement.textContent = currency.name
+        divElement.appendChild(paragraphElement)
+        spanElement.appendChild(divElement)
+        topFiveList.appendChild(spanElement)
+        console.log(topFiveList)
+
+
+    })
+    // const topFiveCurrencies = cryptoCurrencies.filter(cryptoCurrency => {
+    //     return Number(cryptoCurrencies) <=5
+    // })
+    // console.log(topFiveCurrencies)
+})
